a4e5f054b0b8436bdf61765c1d79908dbafb7e40
Andrew R. Villalon <villalon.andrew123@gmail.com>
2025-07-28 11:39:25 +0800

    Update Generating.jsx

--- client/src/components/Generating.jsx
+++ client/src/components/Generating.jsx
@@ -8,7 +8,7 @@ const Generating = ({ className }) => {
       } text-xs md:text-base`}
     >
       {/* <img className="w-5 h-5 mr-4" src={loading} alt="Loading" /> */}
-      With over 20+ Projects for clients and personal use.
+      With over 30+ Projects for clients and personal use.
     </div>
   );
 };
